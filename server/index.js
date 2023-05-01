b5dcaebd124b4be2081f466f911042af97b00ef0
NadiaPia <206i417@gmail.com>
2023-05-01 15:01:16 -0700

    Add states in App.js, joinRoom function and emit-on event btw client and server

--- server/index.js
+++ server/index.js
@@ -15,9 +15,15 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
+    console.log(`User connected: ${socket.id}`);
 
-    socket.io("disconnect", () => {
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+
+    });
+
+    socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     });
 });
